908c66863d23270251c2f190ba4e0bf4955e7ffa
holliemaethomas <holliemaethomas85@gmail.com>
2018-09-08 18:04:39 -0700

    tABLECONNECTED

--- patients.sql
+++ patients.sql
@@ -1,11 +1,8 @@
 CREATE TABLE IF NOT EXISTS
-    patients (
+    patients(
       id SERIAL PRIMARY KEY,
       name VARCHAR(255),
-      weight VARCHAR(255),
-      height VARCHAR(255),
       gender VARCHAR(255),
-      birthdate VARCHAR(255),
-      diagnosis VARCHAR(255),
-      answers TEXT
+      DOB VARCHAR(255),
+      diagnosis VARCHAR(255)
     );
--- server.js
+++ server.js
@@ -24,13 +24,37 @@ app.set('view engine', 'ejs');
 // Gets and posts
 app.get('/', getIndex);
 app.get('*', getError);
+app.post('/patients',addNewPatient);
 
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
 
 // Helper functions
-function getIndex(request, response){
-  response.render('index');
-}
 function getError(request, response) {
   response.render('pages/error');
 }
+
+function addNewPatient(request, response) {
+  let { name, age, gender, DOB } = request.body;
+  let SQL = `INSERT INTO patients
+  (name, age, gender, DOB)
+  VALUES ($1, $2, $3, $4);`;
+  let values = [name, age, gender, DOB];
+  return client.query(SQL, values)
+    .then(() => {
+      SQL = 'SELECT * FROM patients WHERE name=$1, age=$2, gender=$3, DOB=$4;';
+      values = [request.body.name];
+      return client.query(SQL, values)
+        .then(result => response.render('index', { patients: result.rows[0], message: 'sql' }));
+ 
+    })
+    .catch((error)=> console.error(error));
+}
+
+
+function getIndex(request, response){
+  response.render('index');
+}
+
+
+
+
